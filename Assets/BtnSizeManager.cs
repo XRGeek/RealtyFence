f6b2bfdd93bf1a27b4669617331c8e4195361355
sabamajid <saba.majid@euphoria.pk>
2023-03-15 16:32:47 +0500

    UI + lower panel button functionality

---
+++ Assets/BtnSizeManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnSizeManager : MonoBehaviour
+{
+    public GameObject _parentObject;
+
+    public void OnMouseClick()
+    {
+      
+        string name = gameObject.name;
+
+        for (int i = 0; i < _parentObject.transform.childCount; i++)
+        {
+            GameObject childObj = _parentObject.transform.GetChild(i).gameObject;
+
+            if (childObj.name == name)
+            {
+                childObj.transform.GetChild(2).gameObject.SetActive(true);
+                //childObj.GetComponent<Image>().sprite = biggerimg;
+            }
+            else
+            {
+                childObj.transform.GetChild(2).gameObject.SetActive(false);
+                //childObj.GetComponent<Image>().sprite = smallerimg;
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(gameObject.name);
+    }
+
+}
